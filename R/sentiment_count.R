2d6cd7d8cd503f58f9057b0e04534aeeae869b94
FelixWittstrom <felix.wittstrom@ki.se>
2025-04-22 16:18:17 +0200

    Update description for sentiment_count and summarize sentiment

--- R/sentiment_count.R
+++ R/sentiment_count.R
@@ -1,10 +1,16 @@
-#' Count positive and negative sentiment words
+#' sentiment_count
+#'
+#' Count positive and negative sentiment words in a word vector
+#' Gives a list with counts of positive and negative words as output
 #'
 #' @param word_vector Vector of words from the document
 #' @param positive_words Vector of positive words
 #' @param negative_words Vector of negative words
 #' @return A list with counts: positive and negative
-#' @export
+#' @examples
+#' counts <- sentiment_count(words, positive_words, negative_words)
+#' print(counts)
+#'
 sentiment_count <- function(word_vector, positive_words, negative_words) {
   pos_count <- compare_words(word_vector, positive_words)
   neg_count <- compare_words(word_vector, negative_words)
--- R/summarize_sentiment.R
+++ R/summarize_sentiment.R
@@ -1,18 +1,13 @@
+#' summarize_sentiment
+#'
 #' Summarize sentiment with ratio and total word count
 #'
 #' @param word_vector Vector of words from the document
 #' @param positive_words Vector of positive words
 #' @param negative_words Vector of negative words
 #' @return A list with positive count, negative count, ratio, and total words
-#' @export
-#'
 #' @examples
-#' # Example usage:
-#' words <- preprocess_text("C:/Users/felwit/Documents/Offline work/Courses/Intermediate R/Group Exam/Example_negative.txt")
-#' positive_words <- c("hope", "cooperate", "pass")
-#' negative_words <- c("nauseous", "tired", "heavy", "frustrating", "fatigue", "restless", "trapped")
-#' counts <- sentiment_count(words, positive_words, negative_words)
-#' print(counts)
+#' #
 #' summary <- summarize_sentiment(words, positive_words, negative_words)
 #' print(summary)
 summarize_sentiment <- function(word_vector, positive_words, negative_words) {
